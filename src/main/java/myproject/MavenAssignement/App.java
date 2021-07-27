93aacada45c4f7efc127c82e44e9d87973853ea3
Muzhdab <muzhda.shams12@gmail.com>
2021-07-26 22:24:45 -0400

    first commit

---
+++ src/main/java/myproject/MavenAssignement/App.java
@@ -0,0 +1,13 @@
+package myproject.MavenAssignement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
